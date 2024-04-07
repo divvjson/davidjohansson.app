a52d3d2ba4a78378c40391cc7ea42c212edc2ade
David Johansson <david.n.johansson@live.com>
2024-04-07 19:09:59 +0200

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ThemeService } from './services/theme.service';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    MatDividerModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
