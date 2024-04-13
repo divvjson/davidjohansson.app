56b3987b157db79efb5d8263183761ab60070696
David Johansson <david.n.johansson@live.com>
2024-04-13 17:08:27 +0200

    ..

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -10,13 +10,57 @@ export class ThemeService {
     this._isDark = value;
   }
 
+  constructor() {
+    this.setLightTheme();
+  }
+
   public toggle() {
     if (this.isDark) {
-      document.body.classList.remove('dark-theme');
-      this.isDark = false;
+      this.setLightTheme();
     } else {
-      document.body.classList.add('dark-theme');
-      this.isDark = true;
+      this.setDarkTheme();
     }
+  }
+
+  private setLightTheme() {
+    document.body.classList.remove('dark-theme');
+    this.setCSSVariable('--white', '#FFFFFF');
+    this.setCSSVariable('--gray-50', '#FAFAFA');
+    this.setCSSVariable('--gray-100', '#F5F5F5');
+    this.setCSSVariable('--gray-200', '#EEEEEE');
+    this.setCSSVariable('--gray-300', '#E0E0E0');
+    this.setCSSVariable('--gray-400', '#BDBDBD');
+    this.setCSSVariable('--gray-500', '#9E9E9E');
+    this.setCSSVariable('--gray-600', '#757575');
+    this.setCSSVariable('--gray-700', '#616161');
+    this.setCSSVariable('--gray-800', '#424242');
+    this.setCSSVariable('--gray-900', '#212121');
+    this.setCSSVariable('--black', '#000000');
+    this.isDark = false;
+  }
+
+  private setDarkTheme() {
+    document.body.classList.add('dark-theme');
+    this.setCSSVariable('--white', '#000000');
+    this.setCSSVariable('--gray-50', '#212121');
+    this.setCSSVariable('--gray-100', '#424242');
+    this.setCSSVariable('--gray-200', '#616161');
+    this.setCSSVariable('--gray-300', '#757575');
+    this.setCSSVariable('--gray-400', '#9E9E9E');
+    this.setCSSVariable('--gray-500', '#BDBDBD');
+    this.setCSSVariable('--gray-600', '#E0E0E0');
+    this.setCSSVariable('--gray-700', '#EEEEEE');
+    this.setCSSVariable('--gray-800', '#F5F5F5');
+    this.setCSSVariable('--gray-900', '#FAFAFA');
+    this.setCSSVariable('--black', '#FFFFFF');
+    this.isDark = true;
+  }
+
+  public getCSSVariable(propertyName: string) {
+    return getComputedStyle(document.documentElement).getPropertyValue(propertyName);
+  }
+
+  private setCSSVariable(propertyName: string, value: string) {
+    document.documentElement.style.setProperty(propertyName, value);
   }
 }
