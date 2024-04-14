6bb2e2dbbb42f7196281c31fbb08363b6fbf351d
David Johansson <david.n.johansson@live.com>
2024-04-14 19:10:00 +0200

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { ThemeService } from './services/theme.service';
 import { MatDividerModule } from '@angular/material/divider';
 import { ContactComponent } from './components/contact/contact.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    AboutMeComponent,
     ContactComponent,
     HeaderComponent,
     MatDividerModule
